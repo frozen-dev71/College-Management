d28c6c146e494bed110755f43167144f2155471d
miyu-tanaka7 <michaeljohnshane@Gmail.com>
2022-11-15 01:21:33 +0300

    Added file exception handlers

---
+++ College Manager/src/exception/AdminException.java
@@ -0,0 +1,15 @@
+package exception;
+
+@SuppressWarnings("serial")
+public class AdminException extends Exception{
+
+    public AdminException() {
+
+    }
+
+
+    public AdminException(String message) {
+        super(message);
+    }
+
+}
